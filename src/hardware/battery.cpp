76a9427b30ad50c5bbcd288b11c6db8ea0dd0ee6
Kamran Aghlami <oops.legendary@gmail.com>
2023-09-07 11:59:41 +0300

    initial

---
+++ src/hardware/battery.cpp
@@ -0,0 +1,24 @@
+#include "hardware/battery.h"
+
+// #include <Arduino.h>
+
+#include "config.h"
+
+namespace hardware
+{
+    battery battery::s_instance;
+
+    battery::battery()
+    {
+        // pinMode(PIN_BATTERY, INPUT);
+    }
+
+    battery::~battery()
+    {
+    }
+
+    uint32_t battery::voltage_level()
+    {
+        return 0; // analogReadMilliVolts(PIN_BATTERY) * 2;
+    }
+}
