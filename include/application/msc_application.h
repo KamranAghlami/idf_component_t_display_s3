3f0a08204ac211c325ab954ddb33a3884d1e5692
Kamran <oops.legendary@gmail.com>
2023-10-15 16:15:27 +0300

    first msc example

--- include/application/msc_application.h
+++ include/application/msc_application.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <wear_levelling.h>
+
 #include "application/application.h"
 
 class msc_application : public application
@@ -10,4 +12,7 @@ public:
 
     void on_create() override;
     void on_update(float timestep) override;
+
+private:
+    wl_handle_t m_wl_handle;
 };
