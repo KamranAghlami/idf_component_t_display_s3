bbdf70e8a5c7677d1257d7e71c6d2bc12a18d47c
Kamran <oops.legendary@gmail.com>
2024-06-23 18:15:45 +0300

    load wifi config from nvs

--- src/hardware/wifi.cpp
+++ src/hardware/wifi.cpp
@@ -6,24 +6,94 @@
 #include <esp_err.h>
 #include <esp_wifi.h>
 #include <esp_mac.h>
+#include <nvs_flash.h>
 
 namespace hardware
 {
-    enum class wifi_mode
+    enum class wifi_mode : uint8_t
     {
         ACCESS_POINT,
         STATION,
     };
 
     constexpr const char *TAG = "wifi";
-    constexpr const char *WIFI_SSID = "RCLink";
-    constexpr const char *WIFI_PASS = "0123456789";
-    constexpr wifi_mode MODE = wifi_mode::ACCESS_POINT;
-    constexpr uint8_t WIFI_CHAN = 1;
-    constexpr uint8_t MAX_CONN = 2;
+    constexpr const char *AP_DEFAULT_SSID = "RCLink";
+    constexpr const char *AP_DEFAULT_PASS = "0123456789";
+    constexpr uint8_t AP_CHAN = 1;
+    constexpr uint8_t AP_MAX_CONN = 3;
 
     struct wifi_implementation
     {
+        ~wifi_implementation()
+        {
+            if (ssid)
+                delete[] ssid;
+
+            if (password)
+                delete[] password;
+        }
+
+        void load_config()
+        {
+            esp_err_t error = ESP_OK;
+
+            error = nvs_get_u8(nvs_handle, "mode", reinterpret_cast<uint8_t *>(&mode));
+
+            assert(error == ESP_OK || error == ESP_ERR_NVS_NOT_FOUND);
+
+            if (error == ESP_ERR_NVS_NOT_FOUND)
+                mode = wifi_mode::ACCESS_POINT;
+
+            size_t size = 0;
+
+            error = nvs_get_str(nvs_handle, "ssid", nullptr, &size);
+
+            assert(error == ESP_OK || error == ESP_ERR_NVS_NOT_FOUND);
+
+            if (error == ESP_OK)
+            {
+                ssid = new char[size];
+
+                ESP_ERROR_CHECK(nvs_get_str(nvs_handle, "ssid", ssid, &size));
+            }
+            else
+            {
+                ssid = new char[strlen(AP_DEFAULT_SSID) + 1];
+
+                strcpy(ssid, AP_DEFAULT_SSID);
+            }
+
+            size = 0;
+
+            error = nvs_get_str(nvs_handle, "password", nullptr, &size);
+
+            assert(error == ESP_OK || error == ESP_ERR_NVS_NOT_FOUND);
+
+            if (error == ESP_OK)
+            {
+                password = new char[size];
+
+                ESP_ERROR_CHECK(nvs_get_str(nvs_handle, "password", password, &size));
+            }
+            else
+            {
+                password = new char[strlen(AP_DEFAULT_PASS) + 1];
+
+                strcpy(password, AP_DEFAULT_PASS);
+            }
+        }
+
+        void print()
+        {
+            ESP_LOGI(TAG, "mode: %s", mode == wifi_mode::ACCESS_POINT ? "AccessPoint" : "Station");
+            ESP_LOGI(TAG, "ssid: %s", ssid);
+            ESP_LOGI(TAG, "password: %s", password);
+        }
+
+        nvs_handle_t nvs_handle = 0;
+        wifi_mode mode = wifi_mode::ACCESS_POINT;
+        char *ssid = nullptr;
+        char *password = nullptr;
         esp_netif_t *network_interface = nullptr;
     };
 
@@ -114,10 +184,15 @@ namespace hardware
 
     wifi::wifi() : mp_implementation(std::make_unique<wifi_implementation>())
     {
+        ESP_ERROR_CHECK(nvs_open(TAG, NVS_READWRITE, &mp_implementation->nvs_handle));
+
+        mp_implementation->load_config();
+        mp_implementation->print();
+
         ESP_ERROR_CHECK(esp_netif_init());
         ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-        if (MODE == wifi_mode::ACCESS_POINT)
+        if (mp_implementation->mode == wifi_mode::ACCESS_POINT)
             mp_implementation->network_interface = esp_netif_create_default_wifi_ap();
         else
             mp_implementation->network_interface = esp_netif_create_default_wifi_sta();
@@ -130,16 +205,16 @@ namespace hardware
 
         wifi_config_t wifi_config = {};
 
-        if (MODE == wifi_mode::ACCESS_POINT)
+        if (mp_implementation->mode == wifi_mode::ACCESS_POINT)
         {
-            strcpy(reinterpret_cast<char *>(wifi_config.ap.ssid), WIFI_SSID);
-            wifi_config.ap.ssid_len = strlen(WIFI_SSID);
-            wifi_config.ap.channel = WIFI_CHAN;
-            wifi_config.ap.max_connection = MAX_CONN;
+            strcpy(reinterpret_cast<char *>(wifi_config.ap.ssid), mp_implementation->ssid);
+            wifi_config.ap.ssid_len = strlen(mp_implementation->ssid);
+            wifi_config.ap.channel = AP_CHAN;
+            wifi_config.ap.max_connection = AP_MAX_CONN;
 
-            if (strlen(WIFI_PASS))
+            if (strlen(mp_implementation->password))
             {
-                strcpy(reinterpret_cast<char *>(wifi_config.ap.password), WIFI_PASS);
+                strcpy(reinterpret_cast<char *>(wifi_config.ap.password), mp_implementation->password);
 
 #ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
                 wifi_config.ap.authmode = WIFI_AUTH_WPA3_PSK;
@@ -161,8 +236,8 @@ namespace hardware
         }
         else
         {
-            strcpy(reinterpret_cast<char *>(wifi_config.sta.ssid), WIFI_SSID);
-            strcpy(reinterpret_cast<char *>(wifi_config.sta.password), WIFI_PASS);
+            strcpy(reinterpret_cast<char *>(wifi_config.sta.ssid), mp_implementation->ssid);
+            strcpy(reinterpret_cast<char *>(wifi_config.sta.password), mp_implementation->password);
 
             ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
             ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
@@ -177,5 +252,6 @@ namespace hardware
         esp_netif_destroy_default_wifi(mp_implementation->network_interface);
         ESP_ERROR_CHECK(esp_event_loop_delete_default());
         ESP_ERROR_CHECK(esp_netif_deinit());
+        nvs_close(mp_implementation->nvs_handle);
     }
 }
